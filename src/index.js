652c3e93375f87949973584b64a3db6dcab8ebf2
dubkooleg@gmail.com <dubkooleg@gmail.com>
2020-03-19 21:39:09 +0300

    feat: function decode

--- src/index.js
+++ src/index.js
@@ -37,8 +37,23 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+// decode(expr)
+
 function decode(expr) {
     // write your solution here
+    const tenBytes = expr.match(/.{10}/g);
+    let result = tenBytes.map(tenByte => {
+        if(tenByte=='**********')
+            return ' ';
+        const twoBytes = tenByte.match(/.{2}/g);
+        let morseCode = twoBytes.map(o=>{
+            return o=='10'?'.':o=='11'?'-':'';
+        })
+
+        return MORSE_TABLE[morseCode.join('')];
+    });
+    return result.join('');
 }
 
 module.exports = {
